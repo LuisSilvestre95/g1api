e17751106a6b147949a56702915100a453b93e5a
SebastianSanchezTH <juan458.205@gmail.com>
2024-10-02 11:37:16 -0500

    02-10-24-1136-delete.php

--- routes/services/delete.php
+++ routes/services/delete.php
@@ -1,10 +1,64 @@
 <?php
+
+require_once "models/connection.php";
 require_once "controllers/delete.controller.php";
-require_once "models/delete.model.php";
 
-$id = $_GET["id"];
+if (isset($_GET["id"]) && isset($_GET["nameId"])) {
 
-$response = new DeleteModel();
-/*HOLA MUNDO */
-$response->deleteData($table, $id);
-?>
+    $columns = array($_GET["nameId"]);
+
+
+    /*Validar la tabla y las columnas */
+    if (empty(Connection::getColumnsData($table, $columns))) {
+        $json = array(
+            'status' => 400,
+            'results' => 'Error: Fileds in the form do not match the database'
+        );
+        return;
+    }
+
+    /*Peticion DELETE para usuarios autorizados*/
+    if (isset($_GET["token"])) {
+        $tableToken = $_GET["table"] ?? "users";
+        $suffix = $_GET["suffix"] ?? "user";
+
+        $validate = Connection::tokenValidate($_GET["token"], $table, $suffix);
+
+        /*Solicitamos respuesta del controlador para crear datos en cualquier tabla token valido*/
+        if ($validate == "Ok") {
+
+            /*Solicitamos respuesta del controlador para eliminar datos en cualquier tabla */
+            $response = new DeleteController();
+            $response->deleteData($table, $_GET["id"], $_GET["nameId"]);
+        }
+
+        /*Error cuando el token ha expirado*/
+        if ($validate == "Expired") {
+            $json = array(
+                'status' => 303,
+                'results' => 'Error: The token has expired'
+            );
+            echo json_encode($json, http_response_code($json["status"]));
+            return;
+        }
+
+        /*Error cuando el token no coincide con base de datos*/
+        if ($validate == "No autorizado") {
+            $json = array(
+                'status' => 400,
+                'results' => 'Error: The user has no propper authorization'
+            );
+            echo json_encode($json, http_response_code($json["status"]));
+            return;
+        }
+
+        /*Error cuando no envia el token*/
+    } else {
+        $json = array(
+            'status' => 400,
+            'results' => 'Error: Authorization required'
+        );
+        echo json_encode($json, http_response_code($json["status"]));
+        return;
+    }
+}
