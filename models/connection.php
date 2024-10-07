dbe687efec7ba095e27201a6e0f5f8002a8fcf10
alejandraR <alerangelm34@gmail.com>
2024-10-07 08:39:23 -0500

    agregar credenciales bd

--- models/connection.php
+++ models/connection.php
@@ -12,9 +12,9 @@ class Connection{
 
 		$infoDB = array(
 
-			"database" => "panel",
-			"user" => "puentearandauser",
-			"pass" => "rjd%Ijj&CVfs"
+			"database" => "u145597152_grupouno2",
+			"user" => "u145597152_ugrupouno2",
+			"pass" => "1es[Em:$U>l"
 
 		);
 
