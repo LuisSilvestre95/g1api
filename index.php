07c1414c934ace90d465986162552700b5149158
Luis Silvestre <luissilvestre70@gmail.com>
2024-10-09 18:16:50 -0500

    Controller

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Mostrar errores
 
 ini_set('display_errors', 1);
 ini_set("log_errors", 1);
-ini_set("error_log",  "D:/xampp/htdocs/apirest-dinamica/php_error_log");
+ini_set("error_log", "D:/xampp/htdocs/apirest-dinamica/php_error_log");
 
 /*=============================================
 CORS
@@ -15,7 +15,7 @@ CORS
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('content-type: application/json; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8');
 
 /*=============================================
 Requerimientos
@@ -24,4 +24,4 @@ Requerimientos
 require_once "controllers/routes.controller.php";
 
 $index = new RoutesController();
-$index -> index();
+$index->index();
