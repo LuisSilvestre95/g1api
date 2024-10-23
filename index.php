208b1dd51b5fe4410806f81705cfedc13ff8aab5
alejandraR <alerangelm34@gmail.com>
2024-10-23 11:51:34 -0500

    OPTION

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ CORS
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTION');
 header('content-type: application/json; charset=utf-8');
 
 /*=============================================
